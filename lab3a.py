38a4d0ef20c4fe5042ed0315af84c4245b6a8fec
Ebrahim Patel <epatel16@myseneca.ca>
2025-01-30 23:08:34 -0500

    Completed Lab

--- lab3a.py
+++ lab3a.py
@@ -4,17 +4,23 @@
 # Author ID: epatel16
 
 def return_text_value():
-    return "Good Morning Terry" 
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
 
 # return_number_value() function
+
 def return_number_value():
-    return 15  
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
 
 # Main Program
 if __name__ == '__main__':
-    print('python code')
-    text = return_text_value()
-    print(text)
-    number = return_number_value()
-    print(str(number))
+    print('python code')  
+    text = return_text_value()  
+    print(text)  
+    number = return_number_value()  
+    print(str(number))  
 
