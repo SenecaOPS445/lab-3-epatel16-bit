e17310e9d6bc5c10df0165a5a0130e42f5de06ac
Ebrahim Patel <epatel16@myseneca.ca>
2025-01-30 23:11:59 -0500

    Completed Lab

---
+++ lab3b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Lab 3 Part 1 script - functions'''
+# Author ID: epatel16
+
+def sum_numbers(number1, number2):
+    '''Return the sum of number1 and number2'''
+    return int(number1) + int(number2)
+
+def subtract_numbers(number1, number2):
+    '''Return the result of subtracting number2 from number1'''
+    return int(number1) - int(number2)
+
+def multiply_numbers(number1, number2):
+    '''Return the product of number1 and number2'''
+    return int(number1) * int(number2)
+
+if __name__ == '__main__':
+    
+    print(sum_numbers(10, 5))  
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))  
+
